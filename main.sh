c109b855651cac864e463deb92430c8724afb2eb
benKapl <benji1.kaplan@gmail.com>
2024-10-10 15:29:31 +0200

    created  generate_content function in main.py

--- main.sh
+++ main.sh
@@ -1,1 +1,2 @@
 python3 src/main.py
+cd public && python3 -m http.server 8888
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import logging
+import re
 import shutil
 from pathlib import Path
 
-from textnode import TextNode, TextType
+from page_formatter import extract_title, markdown_to_html_node
 
 
 logging.basicConfig(level=logging.DEBUG,
@@ -16,9 +17,15 @@ BASE_DIR = Path(__file__).parent.resolve()
 STATIC_DIR = BASE_DIR.parent / "static"
 PUBLIC_DIR = BASE_DIR.parent / "public"
 
+TEMPLATE_PATH = BASE_DIR.parent / "template.html"
+CONTENT_DIR = BASE_DIR.parent / "content"
+
 
 def main():
     copy_content(source=STATIC_DIR, destination=PUBLIC_DIR)
+    generate_page(from_path=(CONTENT_DIR / "index.md"), 
+                  template_path=TEMPLATE_PATH, 
+                  dest_path=PUBLIC_DIR)
 
 
 def copy_content(source: Path, destination:Path) -> None:
@@ -59,8 +66,30 @@ def delete_content(destination:Path) -> None:
             LOGGER.info(f"DIRECTORY DELETED : {content}")
 
 
+def generate_page(from_path: Path, template_path: Path, dest_path: Path) -> None:
+    LOGGER.info(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    md_content = from_path.read_text()
+    template_content = template_path.read_text()
+
+    title = extract_title(md_content)
+    title_placeholder = "{{ Title }}"
+    html_content = markdown_to_html_node(md_content).to_html()
+    content_placeholder = "{{ Content }}"
+
+    full_html_page = template_content.replace(title_placeholder, title) \
+                                     .replace(content_placeholder, html_content)
+    
+    # html_file = dest_path / f"{from_path.stem}.html"
+    html_file = dest_path / "index.html"
+    html_file.touch
+    html_file.write_text(full_html_page)
+
+    
 if __name__ == "__main__":
     main()
 
 
+
+    # print(TEMPLATE_PATH)
+    # print(MARKDOWN_DIR)
 
